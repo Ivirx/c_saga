9f56fe7907831c2dcd0275dff3cff7fba2e96287
Vivek Kumar <scorpianva@gmail.com>
2024-01-02 22:25:53 +0530

    hello world and variable starting

---
+++ variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 1;                   // integer
+    float y = 6.9;               // floating point
+    char z = 'a';                // character
+    char str[] = "Hello World!"; // string
+    printf("%d\n", x);
+    printf("%f\n", y);
+    printf("%c\n", z);
+    printf("%s\n", str);
+
+    return 0;
+}
